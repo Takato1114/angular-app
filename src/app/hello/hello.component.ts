0cc9893aecad49775bb924eecda54e9d4b402bf8
Takato1114 <tk.umbrella6535@gmail.com>
2021-04-30 18:37:32 +0900

    20210430

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,5 +1,6 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup ,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -14,26 +15,37 @@ import { FormControl, FormGroup } from '@angular/forms';
   })
 export class HelloComponent implements OnInit {
   title:string;
-  message:string;
+  message:string[];
+  lastTarget:any;
+  lastColor:string;
   myControl:FormGroup;
 
-  constructor() {
-   }
+  constructor() { }
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'FormContorolを使う';
+    this.message = ['First item.','Second item', 'Third item.'];
     this.myControl = new FormGroup({
-      control: new FormControl()
+      name: new FormControl('', [Validators.required]),
+      mail: new FormControl('', [Validators.email]),
+      age: new FormControl('', [Validators.min(1), Validators.max(150)])
     });
   }
 
-  doClick() {
-    this.message = '「'+ this.myControl.value + '」と書きましたね。';
+  get name() {return this.myControl.get('name')}
+  get mail() {return this.myControl.get('mail')}
+  get age() {return this.myControl.get('age')}
+
+  doClick(event){
+    if(this.lastTarget != null){
+      this.lastTarget.style.color = this.lastColor;
+      this.lastTarget.style.backgroundColor = 'white';
+    }
+    this.lastTarget = event.target;
+    this.lastColor = event.target.style.color;
+    event.target.style.color = 'white';
+    event.target.style.backgroundColor = 'red';
   }
 
-  onSubmit() {
-    let result = this.myControl.value;
-    this.message = JSON.stringify(result);
+
   }
-}
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
+})
+export class MessageComponent implements OnInit {
+  @Input() content:string[];
+  @Output() action = new EventEmitter<MouseEvent>();
+
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+  doAction(event){
+    this.action.emit(event);
+  }
+
+}
